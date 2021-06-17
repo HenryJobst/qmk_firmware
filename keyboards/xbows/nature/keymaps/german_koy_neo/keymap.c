f09f125cf4fd4ff3c256eedb46be517e52148fbb
Henry Jobst <gitlab@jobst-berlin.de>
2021-06-17 17:35:57 +0200

    change modifier positions for better handling

--- keyboards/xbows/nature/keymaps/german_koy_neo/keymap.c
+++ keyboards/xbows/nature/keymaps/german_koy_neo/keymap.c
@@ -125,9 +125,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,      KC_F7,  KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_F12,   KC_INS,             KC_DEL,
     DE_CIRC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                   KC_6,   KC_7,     KC_8,     KC_9,     KC_0,    NE_MINS,  NE_GRV,   KC_BSPC,
     KC_TAB,   KC_K,     KC_DOT,   KC_O,     KC_COMM,  KC_Z,                   KC_V,   KC_G,     KC_C,     KC_L,     DE_SS,   KC_Y,     DE_ACUT,  KC_RALT,  KC_LNUM,
-    MO_KOY3,  LSFT_TH,  LCTL_TA,  KC_E,     KC_I,     KC_U,       MO_MOUS,    KC_D,  RALT_TT,  RGUI_TR,  RCTL_TN,  RSFT_TS, KC_F,     MO_KOY3,            KC_ENT,
+    MO_KOY3,  LSFT_TH,  LCTL_TA,  KC_E,     KC_I,     KC_U,       MO_MOUS,    KC_D,   RALT_TT,  RGUI_TR,  RCTL_TN,  RSFT_TS, KC_F,     MO_KOY3,            KC_ENT,
     MO_KOY4,  KC_X,     KC_Q,     DE_ADIA,  DE_UDIA,  DE_ODIA,    KC_ENT,     KC_B,   KC_P,     KC_W,     KC_M,     KC_J,    KC_RSFT,            KC_UP,
-    KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,          MO_KOY3, MO_KOY4,KC_SPC,           KC_RALT,            MO_FUNC, KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+    KC_LCTL,  KC_LGUI,  KC_LALT,            MO_KOY3,          KC_SPC, KC_SPC, MO_KOY4,          KC_RALT,            MO_FUNC, KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
   [_KOY2] = LAYOUT(
     _______,  _______,  _______,  _______,  _______,  _______,    _______,    DE_EURO, _______, _______,  _______,  _______, _______,  _______,            _______,
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     DEBUG,    XXXXXXX,  DE_SUP2,  DE_SUP3,  XXXXXXX,  XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,
     XXXXXXX,  K_DOTS,   DE_UNDS,  DE_LBRC,  DE_RBRC,  DE_CIRC,                DE_EXLM, DE_LABK, DE_RABK,  DE_EQL,   DE_AMPR, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
     _______,  DE_BSLS,  DE_SLSH,  DE_LCBR,  DE_RCBR,  DE_ASTR,    XXXXXXX,    DE_QUES, DE_LPRN, DE_RPRN,  DE_MINS,  DE_COLN, DE_AT,    _______,            _______,
-    _______,  DE_HASH,  DE_DLR,   DE_PIPE,  DE_TILD,  DE_CIRC,    XXXXXXX,    DE_PLUS, DE_PERC, DE_DQUO,  DE_QUOT,  DE_SCLN, _______,            _______,
+    _______,  DE_HASH,  DE_DLR,   DE_PIPE,  DE_TILD,  DE_GRV,     XXXXXXX,    DE_PLUS, DE_PERC, DE_DQUO,  DE_QUOT,  DE_SCLN, _______,            _______,
     _______,  _______,  _______,            _______,         _______, _______,_______,          _______,            _______, _______,  _______,  _______,  _______),
 
   [_KOY4] = LAYOUT(
@@ -151,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,  KC_PGUP,  KC_BSPC,  KC_UP,    KC_DEL,   KC_PGDN,                XXXXXXX, DE_7,    DE_8,     DE_9,     KC_PPLS, KC_PMNS,  XXXXXXX,  XXXXXXX,  XXXXXXX,
     XXXXXXX,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_END,     XXXXXXX,    XXXXXXX, LSFT_T4, DE_5,     DE_6,     KC_PCMM, KC_PDOT,  XXXXXXX,            XXXXXXX,
     _______,  KC_ESC,   KC_TAB,   KC_INS,   KC_ENT,   KC_UNDO,    XXXXXXX,    KC_COLN, DE_1,    DE_2,     DE_3,     KC_SCLN, XXXXXXX,            XXXXXXX,
-    XXXXXXX,  XXXXXXX,  KC_P0,              DE_0,             _______, _______,  _______,          XXXXXXX,            XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX),
+    XXXXXXX,  XXXXXXX,  KC_P0,              DE_0,             DE_0, XXXXXXX,  _______,          XXXXXXX,            XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX),
 
   [_MOUSE] = LAYOUT(
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,      XXXXXXX,    XXXXXXX,          XXXXXXX,          XXXXXXX,          XXXXXXX,          XXXXXXX,      XXXXXXX,                  XXXXXXX,
